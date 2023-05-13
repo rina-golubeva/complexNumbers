1b6e5aeb102bee246b6f05b1584d9cebd1e986a0
Arina Golubeva <99752356+rina-golubeva@users.noreply.github.com>
2023-05-13 10:22:02 +0300

    py-file for checking work of module

---
+++ check.py
@@ -0,0 +1,12 @@
+import complexNumbers
+
+a = [1,0]
+b = [2,0]
+c = complexNumbers.plus(a+b)
+print(c)
+d = complexNumbers.minus(a+b)
+print(d)
+e = complexNumbers.times(a+b)
+print(e)
+f = complexNumbers.divide(a+b)
+print(f)
